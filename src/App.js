49b6f31dbae20d44207cb3270d5338ec83ec9e95
HamzaAnwar1998 <anwarhamza919@gmail.com>
2021-06-17 16:25:34 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -7,37 +7,25 @@ import {Login} from './Components/Login'
 import { Home } from './Components/Home'
 import {fs} from './Config/Config'
 
-// function GetDataFromFirestore(){  
-//   const [products, setProducts]=useState([]);
-//   useEffect(()=>{   
-//     const unsubscribe = fs.collection('Products').onSnapshot(snapshot=>{
-//       const newProduct = snapshot.docs.map(doc=>({
-//         ID: doc.id,
-//         ...doc.data()
-//       }))
-//       setProducts(newProduct);
-//     })
-//     return () => unsubscribe && unsubscribe()
-//   },[])
-//   return products
-// }
+function GetDataFromFirestore(){  
+  const [products, setProducts]=useState([]);
+  useEffect(()=>{   
+    const unsubscribe = fs.collection('Products').onSnapshot(snapshot=>{
+      const newProduct = snapshot.docs.map(doc=>({
+        ID: doc.id,
+        ...doc.data()
+      }))
+      setProducts(newProduct);
+    })
+    return () => unsubscribe()
+  },[])
+  return products
+}
 
 export const App = () => {
 
-  // const data = GetDataFromFirestore();
-  // console.log(data); 
-
-  // const [products, setProducts]=useState([]);
-
-  // useEffect(()=>{
-  //   const unsubscribe = fs.collection('Products').onSnapshot(snapshot=>{
-  //     setProducts(snapshot.docs.map(doc=>({
-  //       ID: doc.id,...doc.data()})));
-  //   })
-  //   return ()=>unsubscribe && unsubscribe()
-  // },[])
-
-  // console.log(products);  
+  const data = GetDataFromFirestore();
+  console.log(data); 
   
   return (
     <BrowserRouter>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,24 @@
 import React,{useState,useEffect} from 'react'
 import {fs} from '../Config/Config'
 
-function GetDataFromFirestore(){  
-  const [products, setProducts]=useState([]);
-  useEffect(()=>{   
-    const unsubscribe = fs.collection('Products').onSnapshot(snapshot=>{
-      const newProduct = snapshot.docs.map(doc=>({
-        ID: doc.id,
-        ...doc.data()
-      }))
-      setProducts(newProduct);
-    })
-    return () => unsubscribe && unsubscribe()
-  },[])
-  return products
-}
+// function GetDataFromFirestore(){  
+//   const [products, setProducts]=useState([]);
+//   useEffect(()=>{   
+//     const unsubscribe = fs.collection('Products').onSnapshot(snapshot=>{
+//       const newProduct = snapshot.docs.map(doc=>({
+//         ID: doc.id,
+//         ...doc.data()
+//       }))
+//       setProducts(newProduct);
+//     })
+//     return () => unsubscribe && unsubscribe()
+//   },[])
+//   return products
+// }
 
 export const Home = () => {
-    const data = GetDataFromFirestore();
-    console.log(data); 
+    // const data = GetDataFromFirestore();
+    // console.log(data); 
     return (
         <div>
             Home
